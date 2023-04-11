1af7400c84e80ad68714c959cb6c02e30b5c8595
uchihaleo05 <uchihaleo005@gmail.com>
2023-04-11 21:44:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+
+//                              javaScript
+
+// simple alert
+// alert("this is simple alert");
+
+// -------------------------------------------------
+
+// if we wanna use alert like to show some data in alert then we dont use  " "
+// var firstName = "leo sama";
+// alert(firstName);
+// var actully is variable 
+
+// --------------------------------------------------
+
+//       variable rules 
+
+//  1 : legal names or illegal names
+
+//                 legal
+
+//  var firstNmae 
+// "firstNmae" this is called camelcase
+// lemme explain :  in camelcase we use variabl's second name"s 
+// first latter as capital like ( userInput , userNumber , firstNane )
+// we can't use variabl like ( var firstname e.t.c, ) 
+// if we use variabl like ( var firstname e.t.c, ) it'll show us error
+
+// 2 : allowed special characters in javaScript ( _ or $ )
+// var $nameWith$Dollar = "";
+// var _nameWith_Dollar = "";
+
+//                 Illegal
+
+// var last name 
+// spaces btwn woeds
+// var my#name
+// or reserved words
+
+//               Data types
+// 1 : string 
+// the string is wrap up in " " and used for storing text or characters
+// Var cannot be started with a number
+
+// 2. Number
+// var myLuckyNumber = 8;
+// example = 4, 37, 0, -34, 4.765
+
+// 3 : Boolean
+// var isTodayMonday = true;
+//  true & false
+
+// var data1 = "okairenasai "
+// var data2 = "leo sama!"
+// var data3 = data1 + data2  
+// console.log(data3);
+
+// the : console.log()
+// is used to print the data which we wanna store from user
+
+// var data1 = "5"
+// var data2 = 5
+// var data3 = data1 + data2  
+// console.log(data3);
+
+//  method of changing string into number only if string is number
+// parseInt(data1) + data2
+// Number(data1) + data2 
+//  +data1 + data2
+
+// var data = "uchiha madara "
+// var data2 = data.includes ("madara") 
+// console.log( data2); 
+// this  :  data.include("madara")
+// is used to locate the statement true or false 
+
+//                oprators
+// +  
+// only plus do 2 things  
+// first is simple plus 
+// and second is concadibation
+//  on the other hand else oprators do their on job
+// -
+// *
+// /
+// %
+
+//     BODMAS rule will apply first
+//  when ever we're playing around with these oprators
+// B  :  Brackets
+// O  :  Open or Orders (powers/indices or roots)
+// D  :  Division
+// M  :  Multiplication
+// A  :  Addition
+// S  :  Subtraction
+
+//  ---------------------------------------------------
+
+// var userName = prompt("enter your name")
+// document.write(userName);
+// now prompt is used to take some ualue , number or data from user
+
+// var userName = prompt("enter your name")
+// document.write( "Okairenasai " + userName + " Sama");
+// the  :  document.write 
+// is used to print the data in html or on screen
+
